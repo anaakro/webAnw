d1617ddba6b9287c974d0b5430fc507a60463295
lilyschretter <104341899+lilyschretter@users.noreply.github.com>
2023-01-20 12:14:29 +0100

    test2

--- webAnw/src/main/java/edu/fra/uas/BeanBeispielApplication.java
+++ webAnw/src/main/java/edu/fra/uas/BeanBeispielApplication.java
@@ -18,7 +18,7 @@ public class BeanBeispielApplication {
 
 			@Override
 			public void run(String... args) throws Exception {
-				System.out.println("123");				
+				System.out.println("sdfg");				
 			}
 			
 		};
